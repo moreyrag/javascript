f00bf1687a8199bb23c8946b6efa90982e745ec1
gmoreyra <gmoreyra@hotmail.com>
2020-01-14 19:58:43 -0300

    analisis del problema y pruebas iniciales de implementacion dia 2

--- js/app.js
+++ js/app.js
@@ -20,13 +20,25 @@ var Calculadora = {
   v_largo_valor_display:1,
   v_num_display:0,
   v_str_display:"0",
-  v_teclas_nums: [{0},{1},{2},{3},{4},{5},{6},{7},{8},{9}],
-  
+  v_teclas_nums: [0,1,2,3,4,5,6,7,8,9],
+  v_id_on: "on",
+  v_id_sign: "sign",
+  v_id_dividido: "dividido",
+  v_id_por: "por",
+  v_id_menos: "menos",
+  v_id_punto: "punto",
+  v_id_igual: "igual",
+  v_id_mas: "mas",
+  v_teclas_op: [this.v_id_mas, this.v_id_menos, this.v_id_por, this.v_id_dividido, this.v_id_sign],
+
   init: function(){
+    /*
     var b_mas = document.getElementById("mas")
     b_mas.addEventListener('click', function () {
       console.log("click on mas");
     })
+    */
+    document.sessionStorage.setItem = JSON.stringify({num1:"0"});
     this.v_num_display = this.ejecutaOperacion(this.menos, 3, 5);
     console.log(this.v_num_display);
   },
