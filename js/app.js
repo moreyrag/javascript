49e2a1706cce852f99745747c70e4e1b2d192a3a
gmoreyra <gmoreyra@hotmail.com>
2020-01-31 19:10:24 -0300

    version final testeaeda

--- js/app.js
+++ js/app.js
@@ -28,7 +28,53 @@ var Calculadora = {
   },
   asignarEventoClickTeclas:function (){
     for (var i = 0; i < this.v_teclas.length; i++) {
+      document.getElementById(this.v_teclas[i]).onmousedown=this.reducirTamanio.bind(this);
+    }
+    for (var i = 0; i < this.v_teclas.length; i++) {
+      document.getElementById(this.v_teclas[i]).onmouseup=this.recuperarTamanio.bind(this);
+    }
+    for (var i = 0; i < this.v_teclas.length; i++) {
       document.getElementById(this.v_teclas[i]).onclick=this.registrarTecla.bind(this);
+    }
+  },
+  reducirTamanio: function (event) {
+    switch (event.target.id) {
+      case "mas":
+        document.getElementById(event.target.id).style.width="81%";
+        document.getElementById(event.target.id).style.height="90%";
+        break;
+      case "0":
+      case "1":
+      case "2":
+      case "3":
+      case "punto":
+      case "igual":
+        document.getElementById(event.target.id).style.width="26%";
+        document.getElementById(event.target.id).style.height="56.62px";
+        break;
+      default:
+        document.getElementById(event.target.id).style.width="20%";
+        document.getElementById(event.target.id).style.height="56.62px";
+    }
+  },
+  recuperarTamanio: function (event) {
+    switch (event.target.id) {
+      case "mas":
+        document.getElementById(event.target.id).style.width="90%";
+        document.getElementById(event.target.id).style.height="100%";
+        break;
+      case "0":
+      case "1":
+      case "2":
+      case "3":
+      case "punto":
+      case "igual":
+        document.getElementById(event.target.id).style.width="29%";
+        document.getElementById(event.target.id).style.height="62.91px";
+        break;
+      default:
+        document.getElementById(event.target.id).style.width="22%";
+        document.getElementById(event.target.id).style.height="62.91px";
     }
   },
   registrarTecla:function (event){
@@ -95,7 +141,7 @@ var Calculadora = {
     }
 
     if (v_tecla === "sign"){
-      if (v_ingresando_num1){
+      if (v_ingresando_num1 || (!v_ingresando_num1 && !v_ingresando_num2)){
         v_num1 = v_num1*(-1);
         v_str_resultado = v_num1.toString();
         sessionStorage.setItem('num1',v_str_resultado);
@@ -144,7 +190,7 @@ var Calculadora = {
       }
       if (v_ingresando_num2 && v_str_num2 == "0" && v_tecla === "0") {
         this.imprimirDisplay("0");
-        sessionStorage.setItem('cant_ops',"1");
+        sessionStorage.setItem('cant_ops',"2");
         return;
       }
 
@@ -208,8 +254,6 @@ var Calculadora = {
       sessionStorage.setItem('largo_num2',"0");
       this.self.calcularOperacion();
     }
-
-
   },
   calcularLargo:function(num, largorequerido){
     // retorna objeto con un array[largoTotal, numero truncado al largorequerido]
@@ -222,6 +266,8 @@ var Calculadora = {
 
     var infolargo = {largoTotal:0, numResult:0};
 
+    var numResultStr = numResult.toString();
+
     if (num.toString().includes(".")) {largoTotal--;}
 
     if (largoTotal <= largorequerido){
@@ -230,8 +276,10 @@ var Calculadora = {
     }
     else { // verificar si se puede truncar parte decimal
       if (largoEntero > largorequerido) {
-        infolargo.largoTotal = Infinity;
-        infolargo.numResult = Infinity;
+        // infolargo.largoTotal = Infinity;
+        // infolargo.numResult = Infinity;
+        infolargo.largoTotal = largorequerido;
+        infolargo.numResult = Number(numResultStr.substring(0,largorequerido));
       }
       else { // truncar parte decimal para que cumpla con el largorequerido
         numResult = parteEntera;
@@ -304,6 +352,66 @@ var Calculadora = {
             sessionStorage.setItem('ingresando_num1',false);
             sessionStorage.setItem('ingresando_op',false);
             break;
+            case "menos":
+              result_num = Number(v_num1) - Number(v_num2);
+              v_str_resultado = result_num.toString();
+              this.self.resetear();
+              infoLargo = this.self.calcularLargo(v_str_resultado, 8);
+              v_str_resultado = infoLargo.numResult;
+              v_largo_num1 = infoLargo.largoTotal;
+              sessionStorage.setItem('resultado',v_str_resultado);
+              sessionStorage.setItem('num1',v_str_resultado);
+              sessionStorage.setItem('largo_num1',v_largo_num1);
+              sessionStorage.setItem('num2',v_str_num2);
+              sessionStorage.setItem('largo_num2',v_largo_num2);
+              sessionStorage.setItem('cant_ops',"2");
+              sessionStorage.setItem('oper',v_oper);
+              sessionStorage.setItem('tecla',v_tecla);
+              sessionStorage.setItem('ingresando_num1',false);
+              sessionStorage.setItem('ingresando_op',false);
+              break;
+              case "dividido":
+                if (Number(v_num2) == 0) {
+                  alert("Error");
+                  this.self.resetear();
+                  return;
+                }
+                result_num = Number(v_num1) / Number(v_num2);
+                v_str_resultado = result_num.toString();
+                this.self.resetear();
+                infoLargo = this.self.calcularLargo(v_str_resultado, 8);
+                v_str_resultado = infoLargo.numResult;
+                v_largo_num1 = infoLargo.largoTotal;
+                sessionStorage.setItem('resultado',v_str_resultado);
+                sessionStorage.setItem('num1',v_str_resultado);
+                sessionStorage.setItem('largo_num1',v_largo_num1);
+                sessionStorage.setItem('num2',v_str_num2);
+                sessionStorage.setItem('largo_num2',v_largo_num2);
+                sessionStorage.setItem('cant_ops',"2");
+                sessionStorage.setItem('oper',v_oper);
+                sessionStorage.setItem('tecla',v_tecla);
+                sessionStorage.setItem('ingresando_num1',false);
+                sessionStorage.setItem('ingresando_op',false);
+                break;
+                case "por":
+                  result_num = Number(v_num1) * Number(v_num2);
+                  v_str_resultado = result_num.toString();
+                  this.self.resetear();
+                  infoLargo = this.self.calcularLargo(v_str_resultado, 8);
+                  v_str_resultado = infoLargo.numResult;
+                  v_largo_num1 = infoLargo.largoTotal;
+                  sessionStorage.setItem('resultado',v_str_resultado);
+                  sessionStorage.setItem('num1',v_str_resultado);
+                  sessionStorage.setItem('largo_num1',v_largo_num1);
+                  sessionStorage.setItem('num2',v_str_num2);
+                  sessionStorage.setItem('largo_num2',v_largo_num2);
+                  sessionStorage.setItem('cant_ops',"2");
+                  sessionStorage.setItem('oper',v_oper);
+                  sessionStorage.setItem('tecla',v_tecla);
+                  sessionStorage.setItem('ingresando_num1',false);
+                  sessionStorage.setItem('ingresando_op',false);
+                  break;
+
           default:
 
         }
@@ -312,19 +420,6 @@ var Calculadora = {
       this.self.imprimirDisplay(v_str_resultado);
       return;
     }
-/*
-    if (v_ingresando_op) {
-            sessionStorage.setItem('ingresando_op',true);
-            sessionStorage.setItem('ingresando_num1',false);
-            sessionStorage.setItem('ingresando_num2',true);
-            sessionStorage.setItem('tecla',v_tecla);
-            sessionStorage.setItem('cant_ops',"1");
-            sessionStorage.setItem('oper',v_oper);
-            sessionStorage.setItem('resultado',v_str_resultado);
-            sessionStorage.setItem('num1',v_str_resultado);
-            sessionStorage.setItem('largo_num1',v_largo_num1);
-      }
-      */
   }
 }
 
